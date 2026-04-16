cbbf2f9866ecbe5ee5c310f799479db10f58d852
aditya03-fend <adityaportfolio3@gmail.com>
2026-04-16 20:04:27 +0700

    refactor: update template page to handle params as a Promise for Next.js 15 compatibility

--- src/app/(preview)/preview/[template]/page.tsx
+++ src/app/(preview)/preview/[template]/page.tsx
@@ -1,8 +1,14 @@
 import { data } from "@/lib/dummy/data";
 import { templateRegistry } from "@/lib/templateRegistry";
 
-export default function Page({ params }: { params: { template: string } }) {
-  const Template = templateRegistry[params.template];
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ template: string }>;
+}) {
+  const { template } = await params;
+
+  const Template = templateRegistry[template];
 
   if (!Template) return <div>Template tidak ditemukan</div>;
 
